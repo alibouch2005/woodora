3e18ed4f9a9b0f39a9f2a8e76365ccf4f72f9789
Ali <ali@gmail.com>
2026-08-06 18:03:12 +0100

    Create Woodora header and footer

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class DefaultController extends AbstractController
+{
+
+
+
+    #[Route('/', name: 'home')]
+    public function test(): Response
+    {
+        return $this->render('home/test.html.twig', [
+]);
+    }
+
+
+
+}
